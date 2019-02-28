44348d8d4120591e4db3a7ff8bd7a6670e78d143
Chris Andrew <chrisandrew119@gmail.com>
2019-02-28 20:58:27 +0530

    PEP8

--- project/Radon_And_Inverse_Transform.py
+++ project/Radon_And_Inverse_Transform.py
@@ -7,6 +7,7 @@ Created on Wed Feb 27 00:52:45 2019
 """
 
 import numpy as np
+import pdb
 import matplotlib.pyplot as plt
 
 
@@ -20,39 +21,41 @@ import math
 
 #PROVIDE K-theta in place of image
 def radon_projection(image , theta_arr):
-    
-    
+
+
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4.5))
-    
+
     ax1.set_title("Original")
     ax1.imshow(image, cmap=plt.cm.Greys_r)
-    
+
     #theta = np.linspace(0., 180., max(image.shape), endpoint=False)
-    
+
     sinogram = radon(image, theta_arr ,  circle=True)
     print(sinogram)
-    
+
     ax2.set_title("Radon transform\n(Sinogram)")
     ax2.set_xlabel("Projection angle (deg)")
     ax2.set_ylabel("Projection position (pixels)")
-    
+
     ax2.imshow(sinogram, cmap=plt.cm.Greys_r,
                extent=(0, 180, 0, sinogram.shape[0]), aspect='auto')
-    
+
     #ax2.imshow(sinogram, cmap=plt.cm.Greys_r,extent=(0, 180,-sinogram.shape[0]/2.0, sinogram.shape[0]/2.0) , aspect ='auto')
-    
+
     fig.tight_layout()
     plt.show()
     return sinogram
 
 #INVERSE RADON
-   
-def inverse_radon_reconstruction(sinogram, theta_arr):    
+
+def inverse_radon_reconstruction(sinogram, theta_arr):
 
     reconstruction_fbp = iradon(sinogram, theta_arr, circle=True)
+    plt.imshow(reconstruction_fbp, 'gray')
+    plt.show()
     error = reconstruction_fbp - image
     print('FBP rms reconstruction error: %.3g' % np.sqrt(np.mean(error**2)))
-    
+
     imkwargs = dict(vmin=-0.2, vmax=0.2)
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4.5), sharex=True, sharey=True)
     ax1.set_title("Reconstruction\nFiltered back projection")
@@ -60,13 +63,19 @@ def inverse_radon_reconstruction(sinogram, theta_arr):
     ax2.set_title("Reconstruction error\nFiltered back projection")
     ax2.imshow(reconstruction_fbp - image, cmap=plt.cm.Greys_r, **imkwargs)
     plt.show()
-    
-image = imread(data_dir + "/phantom.png", as_gray=True)
-image = rescale(image, scale=0.4, mode='reflect', multichannel=False)
-theta_arr = np.zeros(36)
-for i in range(36):
-    theta_arr[i] = np.rad2deg(i*np.pi/36)+math.degrees(math.pi/2)  
-    
 
-sinogram_project = radon_projection(image,theta_arr)
-reconstruction = inverse_radon_reconstruction(sinogram_project,theta_arr)
+# image = imread(data_dir + "/phantom.png", as_gray=True)
+# image = rescale(image, scale=0.4, mode='reflect', multichannel=False)
+x, y = np.meshgrid(np.linspace(-1,1,10), np.linspace(-1,1,10))
+d = np.sqrt(x*x+y*y)
+sigma, mu = 1.0, 0.0
+image = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )
+num_angles = 36
+theta_arr = np.zeros(num_angles)
+for i in range(num_angles):
+    theta_arr[i] = np.rad2deg(i*np.pi/num_angles)+math.degrees(math.pi/2)
+
+
+sin = radon_projection(image, theta_arr)
+# pdb.set_trace()/
+reconstruction = inverse_radon_reconstruction(sin,theta_arr)
--- project/initial_estimation.py
+++ project/initial_estimation.py
@@ -1,43 +1,46 @@
 import torch
 import cv2
+import pdb
 import numpy as np
 import torch.nn.functional as F
 
 
 def initialize_LK(blur_img, iterations=5, learning_rate=0.001):
+    """Value for latent image and kernel."""
     latent_img = blur_img.copy()
     latent_img = np.reshape(latent_img, (1, 1, latent_img.shape[0], latent_img.shape[1]))
     latent_img = torch.from_numpy(latent_img)
-    latent_img = latent_img.type('torch.FloatTensor').cuda()
+    latent_img = latent_img.type('torch.FloatTensor')
     latent_img.requires_grad = True
 
     blur_img = np.reshape(blur_img, (1, 1, blur_img.shape[0], blur_img.shape[1]))
     blur_img = torch.from_numpy(blur_img)
-    blur_img = blur_img.type('torch.FloatTensor').cuda()
+    blur_img = blur_img.type('torch.FloatTensor')
 
     # sobel filter
-    a = torch.Tensor([[1, 0, -1],
-    [2, 0, -2],
-    [1, 0, -1]]).cuda()
-    a = a.view((1,1,3,3))
+    s_x = torch.Tensor([[1, 0, -1],
+                       [2, 0, -2],
+                       [1, 0, -1]])
+    s_x = s_x.view((1, 1, 3, 3))
 
-    b = torch.Tensor([[1, 2, 1],
-    [0, 0, 0],
-    [-1, -2, -1]]).cuda()
-    b = b.view((1,1,3,3))
+    s_y = torch.Tensor([[1, 2, 1],
+                       [0, 0, 0],
+                       [-1, -2, -1]])
+    s_y = s_y.view((1, 1, 3, 3))
 
-    conv = torch.nn.Conv2d(1, 1, (31, 31), stride=1, padding=15, bias=False).cuda()
+    conv = torch.nn.Conv2d(1, 1, (31, 31), stride=1, padding=15, bias=False)
     torch.nn.init.normal_(conv.weight, mean=0, std=1)
+    pdb.set_trace()
 
     for i in range(iterations):
-        if i%2==1:
+        if i % 2 == 1:
             out1 = conv(latent_img)
-            norm1 = torch.norm( (blur_img-out1), 2 ).cuda()
+            norm1 = torch.norm((blur_img-out1), 2)
 
-            #calculating total variation as regularization term.
-            Gx = F.conv2d(conv.weight, a)
-            Gy = F.conv2d(conv.weight, b)
-            G =  torch.sum(torch.sqrt(torch.pow(Gx,2)+ torch.pow(Gy,2)))
+            # calculating total variation as regularization term.
+            Gx = F.conv2d(conv.weight, s_x)
+            Gy = F.conv2d(conv.weight, s_y)
+            G = torch.sum(torch.sqrt(torch.pow(Gx, 2) + torch.pow(Gy, 2)))
 
             energy = norm1 + G
             conv.zero_grad()
@@ -47,17 +50,14 @@ def initialize_LK(blur_img, iterations=5, learning_rate=0.001):
                 for param in conv.parameters():
                     param.data -= learning_rate*param.grad
 
-            for param in conv.parameters():
-                param.requires_grad = False
-            latent_img.requires_grad = True
         else:
             out1 = conv(latent_img)
-            norm1 = torch.norm( (blur_img-out1), 2 ).cuda()
+            norm1 = torch.norm((blur_img-out1), 2)
 
-            #calculating total variation as regularization term.
-            Gx = F.conv2d(latent_img, a)
-            Gy = F.conv2d(latent_img, b)
-            G =  torch.sum(torch.sqrt(torch.pow(Gx,2)+ torch.pow(Gy,2)))
+            # calculating total variation as regularization term.
+            Gx = F.conv2d(latent_img, s_x)
+            Gy = F.conv2d(latent_img, s_y)
+            G = torch.sum(torch.sqrt(torch.pow(Gx, 2) + torch.pow(Gy, 2)))
             energy = norm1 + G
 
             conv.zero_grad()
@@ -65,14 +65,6 @@ def initialize_LK(blur_img, iterations=5, learning_rate=0.001):
 
             with torch.no_grad():
                 latent_img -= learning_rate*latent_img.grad
-
-            for param in conv.parameters():
-                param.requires_grad = True
-            latent_img.requires_grad = False
-
-    latent_img.requires_grad=False
-    for param in conv.parameters():
-                param.requires_grad = False
 
     latent_img = latent_img.cpu().numpy()[0][0]
     kernel = conv.weight.cpu().numpy()[0][0]
